3bafc1c40a5f476497a64a213d9f3150b3fc2e09
cgcladera <cgcladera@gmail.com>
2013-10-28 15:53:54 +0100

    Remote feed source

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,9 +1,25 @@
 var Feed = function(options, context){
+    var self = this;
     this.context = context;
     this.options = options;
-    
+    this.running = false;
+    console.log("Loading source...");
+    console.log("Source typeof: "+ typeof this.options.source);
+    if(typeof this.options.source === "object"){
+        console.log("Source array");
+        //Render comments
+        $.each(this.options.source, function(key, comment){
+            self.add(comment);
+        });
+    }else if(typeof this.options.source === "string") {
+        this.running = true;
+        self._get();
+    }else {
+        console.error("Invalid source type");
+    }
     this.context.addClass("feed");
 };
+
 Feed.prototype.add = function(comment){
     var box = $('<div class="comment"></div>');
     box.append('<div class="comment author">'+comment.author || ''+'</div>');
@@ -12,4 +28,34 @@ Feed.prototype.add = function(comment){
     this.context.prepend(box);
     box.fadeIn('slow');
     return this;
+};
+Feed.prototype.refresh = function(){
+    this._get();
+};
+Feed.prototype.pause = function(){
+    this.running = false;
+};
+Feed.prototype.resume = function(){
+    this.running = true;
+    this._get();
+};
+Feed.prototype._get = function(){
+    if(typeof this.options.source === "string"){
+        var self = this;
+        $.ajax({
+            url: self.options.source,
+            dataType: 'json'
+        }).done(function(feed){
+            $.each(feed, function(key, comment){
+                self.add(comment);
+            });
+            if(self.running && self.options.refreshDelay > 0){
+                setTimeout(function(){
+                    if(self.running){
+                        self._get();
+                    }
+                },self.options.refreshDelay*1000);
+            }
+        });
+    }
 };
--- src/js/setup.js
+++ src/js/setup.js
@@ -1,5 +1,8 @@
 $.widget("ui.feed", {
-    options: {},
+    options: {
+        source: [],
+        refreshDelay: 0
+    },
     _create: function(){
         this.feed = new Feed(this.options, this.element);
     },
@@ -10,5 +13,14 @@ $.widget("ui.feed", {
     },
     add: function(comment){
         this.feed.add(comment);
+    },
+    refresh: function(){
+        this.feed.refresh();
+    },
+    pause: function(){
+        this.feed.pause();
+    },
+    resume: function(){
+        this.feed.resume();
     }
 });
